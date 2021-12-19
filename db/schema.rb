16e62839f1c574bba5b83dcaec0dc30b11ee5705
EC2 Default User <ec2-user@ip-172-31-11-64.ec2.internal>
2021-12-19 12:01:21 +0000

    [Finish]3-9

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_12_15_152233) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
